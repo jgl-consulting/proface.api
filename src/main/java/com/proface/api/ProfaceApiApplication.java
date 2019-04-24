488112d633485eb338812a073e373948fffbd9e6
Luis Chávez <luis.chavez24@unmsm.edu.pe>
2019-04-24 01:00:15 -0500

    Base de la API REST

---
+++ src/main/java/com/proface/api/ProfaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.proface.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfaceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proface/api/ProfaceApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.proface.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfaceApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
