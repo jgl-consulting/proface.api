d98206990584e11987c3c21529f062b25736cbdb
Luis Chávez <luchor13@gmail.com>
2019-08-17 17:18:49 -0500

    Filter user roles if they're active

--- src/main/java/com/proface/api/entities/User.java
+++ src/main/java/com/proface/api/entities/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Where;
+import org.hibernate.annotations.WhereJoinTable;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -46,10 +47,11 @@ public class User {
 	private String email;
 
 	@ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.MERGE })
-	@Where(clause="active = 1 ")
-	@JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = {
-			@JoinColumn(name = "role_id") })
 
+	@JoinTable(name = "user_role",
+			joinColumns = { @JoinColumn(name = "user_id") },
+			inverseJoinColumns = { @JoinColumn(name = "role_id")})
+	@WhereJoinTable(clause="active = 1 ")
 	private List<Role> roles;
 
 }
